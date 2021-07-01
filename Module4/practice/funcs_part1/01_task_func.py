c6752f3df8640868a5baadfb9b70ba34fc1fc908
Alexkomkov213 <86655194+Alexkomkov213@users.noreply.github.com>
2021-07-01 12:12:48 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -1,11 +1,12 @@
 # Напишите функцию, возвращающую наибольшее из двух чисел
 
 def max2(n1, n2):
-    # TODO: your code here
-    pass
+    if n1 > n2:
+        print(n1)
+    else:
+        print(n2)
 
 
-# Тестируем функцию
 print(max2(5, 6))
 print(max2(-10, -12))
 print(max2(2.5, 2.6))
